3f58306e250a4801a334ff6892ece1d84c886da7
Spirit <spirit@programmer.net>
2024-08-25 18:45:17 +0300

    use full paths

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const { join } = require('path')
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.use(express.static("public"))
+app.use(express.static(join(__dirname, "public")))
 
 app.listen(PORT, () => {
 	console.log(`Listening at http://localhost:${PORT}`)
