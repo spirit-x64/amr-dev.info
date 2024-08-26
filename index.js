a13e01f8a465d909adacea6303b6aec9872f5400
Spirit <spirit@programmer.net>
2024-08-26 17:52:09 +0300

    add compression

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const compression = require('compression')
 const express = require("express")
 const { join } = require('path')
 const app = express()
 const PORT = process.env.PORT || 8080
+
+app.use(compression())
 
 app.use(express.static(join(__dirname, "public")))
 
