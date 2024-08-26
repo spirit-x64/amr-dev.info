5706a804de25bab4da0aa553444cad610f68511c
Spirit <spirit@programmer.net>
2024-08-26 16:43:04 +0000

    add caching

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const PORT = process.env.PORT || 8080
 
 app.use(compression())
 
-app.use(express.static(join(__dirname, "public")))
+app.use(express.static(join(__dirname, "public")), { 
+	maxAge: 600000 // 10 min
+})
 
 app.listen(PORT, () => {
 	console.log(`Listening at http://localhost:${PORT}`)
